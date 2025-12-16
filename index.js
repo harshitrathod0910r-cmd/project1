fdfc5363c0cfc1c82aa99d61f5e5eea4de8a8fc6
harshitrathod0910r-cmd <harshitrathod0910r@gmail.com>
2025-12-16 16:11:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+//que1.Write a C program to enter two numbers and find their sum.(javascript)
+
+//   a=Number(prompt("enter the value of a"))
+//   b=Number(prompt("enter the value of b"))
+//   console.log( "the sum is="+(a+b))
+
+//que2.Write a C program to enter two numbers and perform all arithmetic operations.(javascript)
+
+//   a=Number(prompt("enter the value of a"))
+//   b=Number(prompt("enter the value of b"))
+//   console.log( "the sum is="+(a+b))
+//   console.log( "the sub is="+(a-b))               
+//   console.log( "the mul is="+(a*b))
+//   console.log( "the div is="+(a/b))
+
+//que3.Write a C program to enter length and breadth of a rectangle and find its perimeter.(javascript)
+
+// length=Number(prompt("enter the value of length"))
+// breadth=Number(prompt("enter the value of breadth"))        
+// console.log( "the peramiter is="+2*(length+breadth))
+
+//que4.Write a C program to enter length and breadth of a rectangle and find its area.(javascript)
+
+// length=Number(prompt("enter the value of length"))
+// breadth=Number(prompt("enter the value of breadth"))         
+//  console.log( "the area is="+(length*breadth))
+
+//que5.Write a C program to enter radius of a circle and find its diameter, circumference and area.(javascript)
+
+// radius=Number(prompt("enter the value of radius"))
+// console.log("diameter is"+(2*radius))
+// console.log("circumference is"+(2*3.14*radius))             
+// console.log("area is"+(2*(radius*radius)))
+
+//que6.Write a C program to enter length in centimeter and convert it into meter and kilometer.(javascript)
+
+// cm=Number(prompt("enter the cintimeter"))
+// console.log("cm in meter is"+(cm/100))                      
+// console.log("cm in kilometer is"+(cm/10000))
+
+//que7.Write a C program to enter temperature in Celsius and convert it into Fahrenheit.(javascript)
+
+// celsius=Number(prompt("enter the celsius"))
+// console.log("fahrenhit is"+(celsius * 9 / 5) + 32)
+
+//que8.Write a C program to enter temperature in Fahrenheit and convert to Celsius.(javascript)
+
+// fahrenheit=Number(prompt("enter the fahrenhit"))
+// console.log("fahrenhit is"+ (fahrenheit - 32) * 5 / 9)
+
+//que9.Write a C program to enter two angles of a triangle and find the third angle.(javascript)
+
+// first=Number(prompt("enter the first angle"))
+// second=Number(prompt("enter the second angle"))
+// console.log("third angle is"+(180 - (first + second)))
+
+//que10.Write a C program to enter P, T, R and calculate Simple Interest.(javascript)
+
+// p=Number(prompt("enter the principle"))
+// r=Number(prompt("enter the rate"))
+// t=Number(prompt("enter the time"))
+// console.log("simple interest is"+(p * t * r) / 100)
+
+
---
+++ index1.ts
@@ -0,0 +1,80 @@
+//que1.Write a C program to enter two numbers and find their sum.
+
+// var a:number=Number(prompt("enter the value a"));
+// var b:number=Number(prompt("enter the value b"));
+// var sum:number=a+b
+// console.log(sum)
+
+//que2.Write a C program to enter two numbers and perform all arithmetic operations.
+
+// var a:number=Number(prompt("enter the value a"));
+// var b:number=Number(prompt("enter the value b"));
+// var sum:number=a+b
+// var sub:number=a-b
+// var mul:number=a*b                                  
+// var div:number=a/b
+// console.log(sum)
+// console.log(sub)
+// console.log(mul)
+// console.log(div)
+
+//que3.Write a C program to enter length and breadth of a rectangle and find its perimeter.
+
+// var length:number=Number(prompt("enter the value of length"));
+// var width:number=Number(prompt("enter the value width"));
+// var peramiter:number=2*(length+width)
+// console.log(peramiter)
+
+//que4.Write a C program to enter length and breadth of a rectangle and find its area.
+
+// var length:number=Number(prompt("enter the value of length"))
+// var breadth:number=Number(prompt("enter the value of breadth"))  
+// var area:number=length*breadth       
+// console.log( "the area is="+area)
+
+//que5.Write a C program to enter radius of a circle and find its diameter, circumference and area.
+
+
+//  var r:number=Number(prompt("enter the radius:"))
+//  var diameter = 2 * r;
+//  var circumference = 2 * 3.14 * r;
+//  var area = 3.14 * (r * r);
+//  console.log("diameter is :",diameter)
+//  console.log("circumference is :",circumference)
+//  console.log("area is :",area)
+
+
+//que6.Write a C program to enter length in centimeter and convert it into meter and kilometer.
+
+// var cm:number=Number(prompt("enter the cintimeter"))
+// console.log("cm in meter is"+(cm/100))                      
+// console.log("cm in kilometer is"+(cm/10000))
+
+//que7.Write a C program to enter temperature in Celsius and convert it into Fahrenheit.
+
+// var cel:number=Number(prompt("enter the Celsius "));
+// var fahrenheit = (cel * 9 / 5) + 32;
+// console.log("farahenhit is=",fahrenheit)
+        
+//que8.Write a C program to enter temperature in Fahrenheit and convert to Celsius.(javascript)
+ 
+// var fah:number=Number(prompt("enter the fahrenheit"));
+// var cel = (fah - 32) * 5 / 9;
+// console.log("Celsius is=",cel)
+
+
+//que9.Write a C program to enter two angles of a triangle and find the third angle.
+        
+// var a:number=Number(prompt("enter the value of first angle:"));
+// var b:number=Number(prompt("enter the value of second angle:"));
+// var c=180-(a+b);
+// console.log("third angle is a:",c);
+
+//que10.Write a C program to enter P, T, R and calculate Simple Interest.(javascript)
+       
+//  var p:number=Number(prompt("enter the principle:"));
+//  var t:number=Number(prompt("enter the time:"));
+//  var r:number=Number(prompt("enter the rate:"));
+//  var si=(p*t*r)/100;
+//  console.log("simple intrest is =",si);
+         
